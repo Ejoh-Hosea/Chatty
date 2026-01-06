27211ad8b80196aedd616a3f29eed8a3ca3765f7
Hosea54 <ejohhoseamwongwe@gmail.com>
2026-01-06 16:41:31 -0500

    tailwind config

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -11,3 +11,4 @@ const router = express.Router();
 router.get("/users", protectRoute, getUsersForSidebar);
 router.get("/:id", protectRoute, getMessages);
 router.post("/send/:id", protectRoute, sendMessage);
+export default router;
