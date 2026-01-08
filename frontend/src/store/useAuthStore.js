8a13759dcc1c03d81d64e1fd150efcd1ccd1670c
Hosea54 <ejohhoseamwongwe@gmail.com>
2026-01-08 11:57:53 -0500

    profile page complete

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -6,6 +6,7 @@ export const useAuthStore = create((set) => ({
   authUser: null,
   isSigningUp: false,
   isLoggingIn: false,
+  isUpdatingProfile: false,
   isCheckingAuth: true,
   checkAuth: async () => {
     try {
@@ -24,7 +25,6 @@ export const useAuthStore = create((set) => ({
       const res = await axiosInstance.post("/auth/signup", data);
       toast.success("Account created successfully");
       set({ authUser: res.data });
-      toast.success("Account created successfully");
     } catch (error) {
       toast.error(error.response.data.message);
     } finally {
@@ -50,6 +50,19 @@ export const useAuthStore = create((set) => ({
       toast.success("Logged out successfully");
     } catch (error) {
       toast.error(error.response.data.message);
+    }
+  },
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("Error in update profile:", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
     }
   },
 }));
