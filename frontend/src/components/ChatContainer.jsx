70fca77e156472957659640c387da9d5039056ab
Hosea54 <ejohhoseamwongwe@gmail.com>
2026-01-12 09:02:24 -0500

    chat Header component added

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from "react";
+import { useChatStore } from "../store/useChatStore";
+import ChatHeader from "./ChatHeader";
+import MessageInput from "./MessageInput";
+
 const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+  useEffect(() => {
+    getMessages(selectedUser._id, getMessages);
+  }, [selectedUser._id, getMessages]);
+  if (isMessagesLoading) return <div>Loading...</div>;
+  return (
+    <div className="flex-1 flex flex-fol overflow-auto">
+      <ChatHeader />
+      <p>Messages...</p>
+      <MessageInput />
+    </div>
+  );
 };
 export default ChatContainer;
