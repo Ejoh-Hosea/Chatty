a70f9b23656e5b0ce6db0c23416ddeb582b3ab27
Hosea54 <ejohhoseamwongwe@gmail.com>
2026-01-14 00:29:01 -0500

    project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import path from "path";
 import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
@@ -22,6 +24,14 @@ app.use(
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 server.listen(5000, () => {
   console.log("server is running on port" + PORT);
   connectDB();
